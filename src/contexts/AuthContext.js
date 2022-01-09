258163955203139f656f6182846770a1a0667088
fearm3 <ibraaa1628@gmail.com>
2022-01-09 16:13:58 +0300

    add context

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,13 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export function useAuthContext() {
+  return useContext(AuthContext);
+}
+
+const AuthContextProvider = ({ children }) => {
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+};
+
+export default AuthContextProvider;
---
+++ src/redux/reducers/authReducer.js
@@ -0,0 +1,24 @@
+import { SET_LOADING_TRUE, SET_LOADING_FALSE } from "../types/authTypes";
+
+const initialState = {
+  loading: false,
+};
+
+export const authReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case SET_LOADING_TRUE:
+      return {
+        ...state,
+        loading: true,
+      };
+    case SET_LOADING_FALSE:
+      return {
+        ...state,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
